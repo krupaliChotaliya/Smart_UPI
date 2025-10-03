91e83db1cd070bfdf6f711e9108c42c7b70e1916
Krupali Chotaliya <126382587+krupaliChotaliya@users.noreply.github.com>
2025-10-03 14:17:11 +0530

    Update server.js

--- BB/server.js
+++ BB/server.js
@@ -53,4 +53,9 @@ app.use(express.static(path.join(__dirname, '')));
 //   res.sendFile(path.join(__dirname, '', 'index.html'));
 // });
 
-app.listen(56215, () => console.log("Server running on port 56215"));
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
